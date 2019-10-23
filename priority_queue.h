f53c4b421bf2be32d13005ad93ff419cc35fff32
hmpark94 <hmpark94@users.noreply.github.com>
2019-10-23 15:16:44 +0900

    Update priority_queue.h

--- priority_queue.h
+++ priority_queue.h
@@ -9,16 +9,16 @@ namespace code_review {
 	
 template <typename T>
 class TemplatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		TemplatePriorityQueue();
-		~TemplatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+  private:
+    std::priority_queue<T> storage;
+  public:
+    TemplatePriorityQueue();
+    ~TemplatePriorityQueue();
+    bool empty() const;
+    const T& top() const;
+    int size();
+    void push(const T&);
+    void pop();
 };
 
 } // namespace code_review
